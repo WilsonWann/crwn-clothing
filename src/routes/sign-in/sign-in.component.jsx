2ec854fa41d9ad5d7f79b429095bc96bd83ee65e
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-03-18 11:38:24 +0800

    added auth and redirect component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,21 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
+
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
 
 const SignIn = () => {
+
+  const getResponseAfterRedirect = async () => {
+    const response = await getRedirectResult(auth)
+
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user)
+    }
+  }
+
+  useEffect(() => {
+    getResponseAfterRedirect()
+  }, []);
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
@@ -12,6 +27,9 @@ const SignIn = () => {
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
+      </button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
       </button>
     </div>
   )

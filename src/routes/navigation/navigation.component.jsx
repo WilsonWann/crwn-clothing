6b425410b1d90052095be1d65a14addc228db65d
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-03-19 16:38:26 +0800

    centrlized auth by using observer function onAuthStateChanged

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,12 +8,7 @@ import { signOutUser } from '../../utils/firebase/firebase.utils'
 import "./navigation.styles.scss"
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext)
-  // console.log("🚀 ~ Navigation ~ currentUser:", currentUser)
-  const signOutHandler = async () => {
-    await signOutUser()
-    setCurrentUser(null)
-  }
+  const { currentUser } = useContext(UserContext)
 
   return (
     <>
@@ -27,7 +22,7 @@ const Navigation = () => {
           </Link>
           {
             currentUser ? (
-              <span className="nav-link" onClick={signOutHandler}>
+              <span className="nav-link" onClick={signOutUser}>
                 Sign Out
               </span>
             ) : (

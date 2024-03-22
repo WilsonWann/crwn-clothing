3660814b2e2f06b8a7619cb6544fa178198e9688
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-03-22 17:45:45 +0800

    added reselect and optimizing selectCategoriesMap, additionally customizing loggerMiddleware

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,31 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, Fragment } from "react"
+import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 
-import { useSelector } from "react-redux"
+import ProductCard from "../../components/product-card/product-card.component"
 import { selectCategoriesMap } from "../../store/categories/categories.selector"
 
-import "./category.styles.scss"
-import ProductCard from "../../components/product-card/product-card.component"
+import { CategoryContainer, Title } from "./category.styles.js"
 
 const Category = () => {
-
   const { category } = useParams()
+  console.log('render/re-rendering category component')
   const categoriesMap = useSelector(selectCategoriesMap)
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
+    console.log('effect fired calling setProducts')
     setProducts(categoriesMap[category])
   }, [category, categoriesMap]);
 
   return (
-    <>
-      <h2 className="category-title" >{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {products && products.map(product =>
-          <ProductCard key={product.id} product={product} />
-        )}
-      </div>
-    </>
+    <Fragment>
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
+        {products &&
+          products.map(product =>
+            <ProductCard key={product.id} product={product} />
+          )}
+      </CategoryContainer>
+    </Fragment>
   )
 }
 

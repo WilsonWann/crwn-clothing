e2017c33af6e70df57c7dbcab210466062411247
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-03-25 14:15:18 +0800

    added redux-thunk and create fetch categories actions thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,20 +1,28 @@
 import { useSelector } from "react-redux"
-import { selectCategoriesMap } from "../../store/categories/categories.selector"
+import { selectCategoriesMap, selectCategoriesIsLoading } from "../../store/categories/categories.selector"
 import CategoryPreview from '../../components/category-preview/category-preview.component'
+import Spinner from "../../components/spinner/spinner.component"
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap)
+  const isLoading = useSelector(selectCategoriesIsLoading)
 
   return (
     <>
-      {Object.keys(categoriesMap)
-        .map(title =>
-          <CategoryPreview
-            key={title}
-            title={title}
-            products={categoriesMap[title]}
-          />
-        )}
+      {
+        isLoading
+          ? <Spinner />
+          : (
+            Object.keys(categoriesMap)
+              .map(title =>
+                <CategoryPreview
+                  key={title}
+                  title={title}
+                  products={categoriesMap[title]}
+                />
+              )
+          )
+      }
     </>
   )
 }

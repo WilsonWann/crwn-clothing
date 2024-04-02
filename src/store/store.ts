13939e630f2f2e365049ad1984f8eb05d9919f0f
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-04-02 13:54:01 +0800

    typing store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { compose, legacy_createStore as createStore, applyMiddleware } from "redux"
-import { persistStore, persistReducer } from "redux-persist"
+import { compose, legacy_createStore as createStore, applyMiddleware, Middleware } from "redux"
+import { persistStore, persistReducer, PersistConfig } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import logger from "redux-logger"
 // import { thunk } from "redux-thunk" //* choose either thunk or saga
@@ -9,7 +9,19 @@ import { rootSaga } from "./root-saga"
 
 import { rootReducer } from './root-reducer'
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[]
+}
+
+const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
   whitelist: ['cart']
@@ -23,7 +35,7 @@ const middleWares = [
   process.env.NODE_ENV !== 'production' && logger,
   // thunk,
   sagaMiddleware
-].filter(Boolean)
+].filter((middleware): middleware is Middleware => Boolean(middleware))
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&

6e06c432109d294175d4b87dcb18d73848936e54
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-04-03 17:44:18 +0800

    feat: Add workbox libraries and update manifest
    Added multiple workbox libraries to package.json for service worker functionality. Updated manifest.json to include new images for improved PWA experience. Also, registered service worker in index.js for offline capabilities.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { PersistGate } from "redux-persist/integration/react"
 
 import App from './App';
 import { store, persistor } from "./store/store"
+import * as serviceWorkerRegister from "./serviceWorkerRegistration"
 
 import './index.scss';
 
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -27,4 +28,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+serviceWorkerRegister.register()

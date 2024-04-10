dae92fd0cba1744972df11d958e8f9321649b344
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-04-10 10:23:25 +0800

    add test for dropdown

--- src/routes/navigation/__test__/navigation.component.test.js
+++ src/routes/navigation/__test__/navigation.component.test.js
@@ -35,4 +35,32 @@ describe('Navigation tests', () => {
     const signOutLinkElement = screen.getByText(/sign out/i)
     expect(signOutLinkElement).toBeInTheDocument()
   })
+
+  test('it should not render a cart dropdown if isCartOpen is false', () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: false,
+          cartItems: []
+        }
+      }
+    });
+
+    const dropdownTextElement = screen.queryByText(/Your cart is empty/i)
+    expect(dropdownTextElement).toBeNull()
+  })
+
+  test('it should render a cart dropdown if isCartOpen is true', () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: true,
+          cartItems: []
+        }
+      }
+    });
+
+    const dropdownTextElement = screen.queryByText(/Your cart is empty/i)
+    expect(dropdownTextElement).toBeInTheDocument()
+  })
 })
